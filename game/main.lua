44ae2170d562bfa07e5eaf98133cf2371384b3af
ColinW <colinnw15@gmail.com>
2025-11-20 21:22:50 -0500

    fix transparent

--- game/main.lua
+++ game/main.lua
@@ -1,5 +1,6 @@
 function love.load()
     love.window.setTitle("My Awesome Game")
+    width, height = love.graphics.getDimensions()
     -- Background image
     background = love.graphics.newImage("background.png")
 
@@ -13,5 +14,5 @@ function love.draw()
 
     -- Draw play button
     love.graphics.setBlendMode("alpha")
-    love.graphics.draw(startImage, 100, 100, 0, 0.25, 0.25)
+    love.graphics.draw(startImage, width/2, height/2, 0, 0.25, 0.25)
 end
