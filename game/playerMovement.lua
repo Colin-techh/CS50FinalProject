abff5b0b88c2367a775ae9ec60fb040c1f4a29a2
Levi <levipackfan@gmail.com>
2025-11-20 23:03:26 -0500

    m

--- game/playerMovement.lua
+++ game/playerMovement.lua
@@ -20,12 +20,7 @@ function update(player, yaleEnemy, key_mappings, dt)
 
     player.x = player.x + input.x * player.speed * dt
     player.y = player.y + input.y * player.speed * dt
-
-    -- vX = player.x - yaleEnemy.x
-    -- vY = player.y - yaleEnemy.y
-
-    -- yaleEnemy.x = yaleEnemy.x + (vX / math.sqrt(vX^2 + vY^2)) * yaleEnemy.speed * dt
-    -- yaleEnemy.y = yaleEnemy.y + (vY / math.sqrt(vX^2 + vY^2)) * yaleEnemy.speed * dt
+    
 end
 return {
     update = update
