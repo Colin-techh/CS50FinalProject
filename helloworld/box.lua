f800a8f75e01e6d8fe50e85ea1dab481fa6ab610
ColinW <colinnw15@gmail.com>
2025-11-20 20:04:10 -0500

    a

---
+++ helloworld/box.lua
@@ -0,0 +1,28 @@
+box = {}
+	function box:new(width, height)
+		local direction = math.random()*2*math.pi
+		local newObject = {x = math.random()*width, y = math.random()*height, Vx = math.cos(direction), Vy = math.sin(direction)}
+		self.__index = self
+		return setmetatable(newObject, self)
+	end
+	function box:draw()
+		love.graphics.rectangle("fill", self.x,self.y,10,10)
+	end
+	function box:increase(width, height)
+		self.x = self.x + self.Vx
+		self.y = self.y + self.Vy
+
+		-- lets use polar coordintes
+		local mouseX, mouseY = love.mouse.getPosition()
+		local dx = mouseX - self.x
+		local dy = mouseY - self.y
+
+		local Vmag = math.sqrt(self.Vx * self.Vx + self.Vy * self.Vy)
+		local deltaTheta = math.atan2(self.Vx * dy - self.Vy * dx, self.Vx * dx + self.Vy * dy)
+		
+		local theta = math.atan2(self.Vy , self.Vx)
+		self.Vx = Vmag * math.cos(theta + deltaTheta)
+		self.Vy = Vmag * math.sin(theta + deltaTheta)
+
+		
+	end
---
+++ helloworld/main.lua
@@ -0,0 +1,30 @@
+function love.load()
+	width, height = love.graphics.getDimensions()
+	math.randomseed(os.time())
+	require("box")
+
+	-- List of boxes
+	boxes = {}
+	for i = 1, 50 do
+		boxes[i] = box:new(width, height)
+	end
+end
+function love.mousepressed(x, y, button)
+	-- On button press, send boxes around in random direction
+	for key, value in pairs(boxes) do
+		local direction = math.random() * 2 * math.pi
+		boxes[key].x = math.cos(direction) * width/2 + x
+		boxes[key].y = math.sin(direction) * height/2 + y
+	end
+	
+end
+function love.update()
+	for key, value in pairs(boxes) do
+		boxes[key]:increase(width, height)
+	end
+end
+function love.draw()
+	for key, value in pairs(boxes) do
+		boxes[key]:draw()
+	end
+end
