58803ea9f60f55f77e302e0eb831ade39ac525f8
ColinW <colinnw15@gmail.com>
2025-12-04 20:04:04 -0500

    hrituhrth

--- game/dartmouth.lua
+++ game/dartmouth.lua
@@ -1,7 +1,7 @@
 enemy = require("enemy")
 dartmouth = {}
 function dartmouthEnemy:new(xx, yy)
-    local obj = enemy:new({x=xx, y=yy, width=22, height=28, speed=80, damage=2, knockback=64, health=80, imagePath="assets/enemy3.png"})
+    local obj = enemy:new({x=xx, y=yy, width=22, height=28, speed=80, damage=2, knockback=64, health=10, imagePath="assets/enemy3.png"})
     setmetatable(self, {__index = enemy})
     setmetatable(obj, {__index = self})
     return obj
