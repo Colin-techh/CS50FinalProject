bd906dac7f27b3b657631ec762873215d71593b1
Levi <levipackfan@gmail.com>
2025-12-04 19:39:53 -0500

    m

--- game/smokeCloud.lua
+++ game/smokeCloud.lua
@@ -21,14 +21,14 @@ function smokeCloud:new(x, y, radius)
     return obj
 end
 function smokeCloud:update(dt)
-    -- self.age = self.age + dt
-    -- self.radius = self.radius + dt * 10 -- Expand radius over time
-    -- if self.age >= self.lifetime then
-    --     self.isExpired = true
-    -- end
+    self.age = self.age + dt
+    self.radius = self.radius + dt * 10 -- Expand radius over time
+    if self.age >= self.lifetime then
+    self.isExpired = true
+    end
 end
 function smokeCloud:draw()
-    -- love.graphics.setColor(1, 1, 1, 0.5) -- Set color to white with 50% opacity
-    -- love.graphics.circle("fill", self.x, self.y, self.radius)
-    -- love.graphics.setColor(1, 1, 1, 1) -- Reset color to opaque white 
+    love.graphics.setColor(1, 1, 1, 0.5) -- Set color to white with 50% opacity
+    love.graphics.circle("fill", self.x, self.y, self.radius)
+    love.graphics.setColor(1, 1, 1, 1) -- Reset color to opaque white 
 end
