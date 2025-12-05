b439ac13802d0d9510d8fcd44ea9f05d78297368
ColinW <colinnw15@gmail.com>
2025-12-04 19:46:58 -0500

    rdg

--- game/smokeCloud.lua
+++ game/smokeCloud.lua
@@ -11,7 +11,7 @@ function smokeCloud:new(x, y, radius)
         height = radius * 2,
         isExpired = false,
         damage = 0.5, -- Damage per second
-        lifetime = 3, -- lasts for 5 seconds
+        lifetime = 8, -- lasts for 5 seconds
         imagePath = nil
     })
     self.__index = self
@@ -21,14 +21,14 @@ function smokeCloud:new(x, y, radius)
     return obj
 end
 function smokeCloud:update(dt)
-    -- self.age = self.age + dt
-    -- self.radius = self.radius + dt * 10 -- Expand radius over time
-    -- if self.age >= self.lifetime then
-    --     self.isExpired = true
-    -- end
+    self.age = self.age + dt
+    self.radius = self.radius + dt * 10 -- Expand radius over time
+    if self.age >= self.lifetime then
+        self.isExpired = true
+    end
 end
 function smokeCloud:draw()
-    -- love.graphics.setColor(1, 1, 1, 0.5) -- Set color to white with 50% opacity
-    -- love.graphics.circle("fill", self.x, self.y, self.radius)
-    -- love.graphics.setColor(1, 1, 1, 1) -- Reset color to opaque white 
+    love.graphics.setColor(1, 1, 1, 0.5) -- Set color to white with 50% opacity
+    love.graphics.circle("fill", self.x, self.y, self.radius)
+    love.graphics.setColor(1, 1, 1, 1) -- Reset color to opaque white 
 end
